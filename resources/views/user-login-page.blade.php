67288fb6eab303dde65051ebfe90c156a229723a
AnjitGC741 <97378459+AnjitGC741@users.noreply.github.com>
2023-02-15 00:29:05 +0545

    Revert "contact-branch update"

--- resources/views/user-login-page.blade.php
+++ resources/views/user-login-page.blade.php
@@ -36,7 +36,7 @@
             height: 90%;
         }
         .btn1{
-            height: 41px;
+            height: 40px;
             color: white;
             width: 100%;
             border-style: none;
