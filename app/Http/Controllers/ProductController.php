3d13351ba0e43564cb236378c6d601c2b744eedb
BijayShrestha780 <bjaystha678@gmail.com>
2023-02-26 23:13:10 +0545

    few changes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,15 +1,14 @@
-<?php
+// namespace App\Http\Controllers;
 
-namespace App\Http\Controllers;
+// use Illuminate\Http\Request;
 
-use Illuminate\Http\Request;
-
-class ProductController extends Controller
-{
-    public function index(Request $request)
-    {
-$products = Product::all();
+// class ProductController extends Controller
+// {
+    // public function index(Request $request)
+    // {
+//$products = Product::all();
 //return view(view: 'product.index',compact(var_name 'products'));
 
-    }
-}
+    // }
+// }
+//  -->
